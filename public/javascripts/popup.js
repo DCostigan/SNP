a20e0b0e7db87f7e0239e90c6f0e2a371052a70e
Derek Costigan <dcostiga@umass.edu>
2015-06-06 23:49:38 -0400

    Front end changes to add functionality to the button and list field (not completed yet)

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,3 +1,5 @@
+var list;
+
 function renderHeader(){
     var header = document.getElementById('header');
     var logo = document.createElement('img');
@@ -34,27 +36,67 @@ function renderButton(){
     button.style.alignContent = "center";
     button.style.boxSizing.length = "50px";
     button.style.boxSizing.height = "25px";
-    $.getScript("snp.js", function(){
-
-        buttonAction();
-
-    });
 }
 
 function renderList(){
-
+    list.check(keyPair);
 }
 
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
 
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
 
+    // Fire the loading
+    head.appendChild(script);
+}
+
+var populateList = function (){
+  list = createListClient();
+};
+
+var buttonCreation = function() {
+
+    var addText = $('#add-text');
+    var addButton = $('#add-button');
+
+    var addButtonEvent = new PostButton({
+        view    : addButton,
+        input   : addText
+    });
+
+    addButtonEvent.bind('click', function(event){
+        console.log(this);
+        var text = this.input.val();
+        var injectionProofText = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        list.post(generatePublicKey(), injectionProofText);
+        $('#list-public').append('<li>injectionProofText</li>');
+        this.input.val('');
+        return false;
+    });
+};
 
 document.addEventListener('DOMContentLoaded', function() {
+    console.log('HelloApp!' + '\n');
     renderHeader();
     renderPublic('Your Public Key: ');
     renderTextField();
     renderButton();
-    renderList();
 
-    }, function(errorMessage) {
+    loadScript("/public/javascripts/snp.js", populateList);
+    renderList();
+    loadScript("/public/javascripts/snp.js", buttonCreation);
+
+
+
+}, function(errorMessage) {
       renderStatus('Cannot display settings. ' + errorMessage);
 });
--- public/javascripts/snp.js
+++ public/javascripts/snp.js
@@ -2,6 +2,70 @@
  * Created by Derek on 6/6/2015.
  */
 
-function buttonAction(){
-    alert("Hit buttonAction!");
+function PostButton(config){
+    for (var prop in config){
+        if(config.hasOwnProperty(prop)){
+            this[prop] = config[prop];
+        }
+    }
 }
+
+PostButton.prototype = {
+    bind : function (type, cb) {
+        var that = this;
+        this.view.bind(type, function (event) {
+            cb.call(that, event);
+        });
+    }
+};
+
+function ServerClient(config){
+    for(var prop in config) {
+        if(config.hasOwnProperty(prop)){
+            this[prop] = config[prop];
+        }
+    }
+}
+
+ServerClient.prototype = {
+    keyPairs : [],
+
+    post : function (userPublic, destPublic){
+        $.ajax({
+            type        : 'POST',
+            url         : '/postkeys',
+            data        : {'userPublic' : userPublic, 'destPublic' : destPublic},
+            dataType    : 'json'
+        }).done(function (data) {
+            console.log('Post status: ' + data.status);
+        });
+    },
+
+    check : function (keyPair) {
+        var that = this;
+        $.ajax({
+            type        : 'POST',
+            url         : '/checkkeys',
+            data        : {last : that.keyPairs.length },
+            dataType    : 'json'
+        }).done(function (data) {
+            console.log('Check the rcvd key pairs: ' + JSON.stringify(data));
+
+            //that.keyPairs = that.keyPairs.concat(data);
+            //
+            //that.view.empty();
+            //for(var i=0; i< that.keyPairs.length; i++){
+            //
+            //}
+        });
+    }
+};
+
+var list = new ServerClient({
+    view     : $('ul#list-public')
+});
+
+function createListClient(){
+    return list;
+}
+
