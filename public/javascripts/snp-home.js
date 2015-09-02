e02e2070b4bf61af554f995cae494c773cfced03
Derek Costigan <dcostiga@umass.edu>
2015-09-01 22:13:38 -0400

    Early Implementation of home
    NOTE:
    1) Add User is broken (works on empty address?)
    2) Check homec routes b/c they cause crash?

--- public/javascripts/snp-home.js
+++ public/javascripts/snp-home.js
@@ -26,22 +26,42 @@ function HomeClient(config){
 
 HomeClient.prototype = {
 
-    check : function (uname, password) {
+    check : function (uname) {
         var that = this;
         $.ajax({
-            type        : 'POST',
-            url         : '/checkuser',
-            data        : {'uname' : uname, 'password' : password},
-            dataType    : 'json'
+            type: 'POST',
+            url: '/home/checkuser',
+            data: {'uname': uname},
+            dataType: 'json'
         }).done(function (data) {
             console.log('Valid User: ' + data.status);
-            if(data.status === 'OK') {
-                window.location.assign("http://localhost:3000/home");
+            if (data.status === 'OK') {
+                var li = $('<li>');
+                li.html("<label><input type=&#34;text&#34; value=&#34;uname&#34; class=&#34;form-control&#34; readonly></label><input type=&#34;image&#34; class=&#34;delete-button&#34; src=&#34;/images/delete.png&#34; alt=&#34;Delete&#34;>");
+                that.view.append(li);
             }
-            else{
+            else {
                 alert("Invalid Username and/or Password");
             }
         });
+    },
+
+        remove : function (uname) {
+            var that = this;
+            $.ajax({
+                type        : 'POST',
+                url         : '/home/removefriend',
+                data        : {'uname' : uname},
+                dataType    : 'json'
+            }).done(function (data) {
+                console.log('Valid User: ' + data.status);
+                if(data.status === 'OK') {
+
+                }
+                else{
+                    alert("Invalid Username and/or Password");
+                }
+            });
     }
 };
 
@@ -50,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var username = $('#user-name');
     var addUser = $('#add-user');
     var friendsList = $('#friends-list');
-    var invitationList = $('#invitation-list')
+    var invitationList = $('#invitation-list');
 
     var homec = new HomeClient({
         view : username,
@@ -63,14 +83,32 @@ document.addEventListener('DOMContentLoaded', function () {
         input   : username
     });
 
-    loginButton.createListener('click', function(event){
+    $("ul").on("click", "input.trash-button", function(e){
+        e.preventDefault();
+        //var uname = this;
+        //console.log(uname);
+        //homec.remove(uname);
+        $(this).parent().remove();
+    });
+
+    //$("ul").on("click", "input.accept-button", function(e){
+    //    e.preventDefault();
+    //    homec.accept(this);
+    //    $(this).parent().remove();
+    //});
+    //
+    $("ul").on("click", "input.delete-button", function(e){
+        e.preventDefault();
+        //homec.reject(this);
+        $(this).parent().remove();
+    });
+
+    addButton.createListener('click', function(event){
         console.log(this);
+        console.log("Hit addButton!\n");
         var text = this.input.val();
         var injectionProofUsername = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        //$('#event-name').prop('readonly', true);
-        var text2 = this.input2.val();
-        var injectionProofPassword = text2.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        indexc.check(injectionProofUsername, injectionProofPassword);
+        homec.check(injectionProofUsername);
         return false;
     });
 });
--- routes/home.js
+++ routes/home.js
@@ -14,14 +14,30 @@ router.get('/', function(req, res, next) {
 router.post('/checkuser', function(req, res, next) {
     console.log('Checking for User in Database!\n');
     var name = req.body.uname;
-    var pass = req.body.password;
-    console.log(name + ' ' + pass + "\n");
+    console.log(name + "\n");
 
     //CLEAR CACHED users[]  --OR--  KEEP SMALL USERS CACHE
     //CALL DATABASE FINDUSER
     var foundUser = 1;
     //IF USER IS IN DB PUSH TO CACHE ARRAY
+    //SEND INVITATION TO USER W/ THIS NAME
     var response = ({status: 'INVALID'})
+    if(foundUser)
+        response = ({status: 'OK'});
+    res.json(response);
+});
+
+router.post('/removefriend', function(req, res, next) {
+    console.log('Removing Friend from list!\n');
+    var name = req.body.uname;
+    console.log(name + "\n");
+
+    //CLEAR CACHED users[]  --OR--  KEEP SMALL USERS CACHE
+    //CALL DATABASE FINDUSER
+    var foundUser = 1;
+    //IF USER IS IN DB PUSH TO CACHE ARRAY
+    //REMOVE ASSOCIATION W/ CURRENT USER
+    var response = ({status: 'FAILED'})
     if(foundUser)
         response = ({status: 'OK'});
     res.json(response);
