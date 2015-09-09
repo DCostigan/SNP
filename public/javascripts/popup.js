e854b44a799deb20802ff7ba73e52d0fbed403dd
Derek Costigan <djcostigan777@gmail.com>
2015-09-09 19:52:13 -0400

    Completed Popup Logic until Cookies

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,121 +1,3 @@
-//var list;
-////var buttonTypes;
-//function PostButton(config){
-//    for (var prop in config){
-//        if(config.hasOwnProperty(prop)){
-//            this[prop] = config[prop];
-//        }
-//    }
-//}
-//
-//PostButton.prototype = {
-//    createListener : function (type, cb) {
-//        var that = this;
-//        console.log(this.view);
-//        this.view.on(type, function (event) {
-//            cb.call(that, event);
-//        });
-//    }
-//};
-//
-//function ServerClient(config){
-//    for(var prop in config) {
-//        if(config.hasOwnProperty(prop)){
-//            this[prop] = config[prop];
-//        }
-//    }
-//}
-//
-//ServerClient.prototype = {
-//    keyPairs : [],
-//
-//    post : function (userPublic, destPublic){
-//        $.ajax({
-//            type        : 'POST',
-//            url         : '/postkeys',
-//            data        : {'userPublic' : userPublic, 'destPublic' : destPublic},
-//            dataType    : 'json'
-//        }).done(function (data) {
-//            console.log('Post status: ' + data.status);
-//        });
-//    },
-//
-//    check : function (keyPair) {
-//        var that = this;
-//        $.ajax({
-//            type        : 'POST',
-//            url         : '/checkkeys',
-//            data        : {last : that.keyPairs.length },
-//            dataType    : 'json'
-//        }).done(function (data) {
-//            console.log('Check the rcvd key pairs: ' + JSON.stringify(data));
-//
-//            //that.keyPairs = that.keyPairs.concat(data);
-//            //
-//            //that.view.empty();
-//            //for(var i=0; i< that.keyPairs.length; i++){
-//            //
-//            //}
-//        });
-//    }
-//};
-//
-//function renderHeader(){
-//    var header = document.getElementById('header');
-//    var logo = document.createElement('img');
-//    //logo.src = 'public/images/icon4.png';
-//    logo.setAttribute("src", "/public/images/icon4.png");
-//    logo.setAttribute("height", "64");
-//    logo.setAttribute("width", "64");
-//    logo.setAttribute("alt", "Logo");
-//    var h2 = document.createElement('h2');
-//    var text = document.createTextNode('Social Network Privacy');
-//    h2.appendChild(text);
-//    header.appendChild(logo);
-//    header.appendChild(h2);
-//}
-//
-//function generatePublicKey(){
-//    return "Pending...";
-//}
-//
-//function renderPublic(publicText) {
-//    var public = document.getElementById('public');
-//    public.textContent = publicText + generatePublicKey();
-//    public.style.width = "300px";
-//}
-//
-//function renderTextField(){
-//    var text = document.getElementById('add-text');
-//    text.style.color = "gray";
-//    text.style.boxSizing.length = "200px";
-//}
-//
-////function createButtonTypes(cb){
-////    buttonTypes = {
-////        ADD : document.getElementById('add-button')
-////    }
-////}
-//
-//function renderButton(){
-//    console.log("Hit renderButton\n");
-//    var button = document.getElementById('add-button');
-//    button.style.alignContent = "center";
-//    button.style.boxSizing.length = "50px";
-//    button.style.boxSizing.height = "25px";
-//    initButton();
-//}
-//
-//function renderList(){
-//    if(list !== 'undefined'){
-//        console.log('Rendered List\n');
-//        //list.check(keyPair);
-//    }
-//    else{
-//        console.log('list undefined\n');
-//    }
-//}
-//
 //function loadScript(url, callback) {
 //    // Adding the script tag to the head as suggested before
 //    var head = document.getElementsByTagName('head')[0];
@@ -131,50 +13,6 @@
 //    // Fire the loading
 //    head.appendChild(script);
 //}
-//
-//function initButton() {
-//    console.log("Hit initButton\n");
-//    var addText = document.getElementById('add-text');
-//    var addButton = document.getElementById('add-button');
-//    //var buttonType = buttonTypes.ADD;
-//    //console.log(addButton + '\n');
-//    //console.log(buttonType + '\n');
-//    //if(addButton == buttonType) {
-//    //    console.log("YES\n");
-//    //}
-//
-//    var addButtonEvent = new PostButton({
-//        view    : addButton,
-//        input   : addText
-//    });
-//
-//    //addButton.addEventListener('click', function(){
-//    //    console.log(this);
-//    //    var text = this.input.val();
-//    //    var injectionProofText = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-//    //    list.post(generatePublicKey(), injectionProofText);
-//    //    document.getElementById('list-public').append('<li>injectionProofText</li>');
-//    //    this.input.val('');
-//    //    return false;
-//    //});
-//
-//    addButtonEvent.createListener('click', function(event){
-//        console.log(this);
-//        var text = this.input.val();
-//        var injectionProofText = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-//        list.post(generatePublicKey(), injectionProofText);
-//        document.getElementById('list-public').append('<li>injectionProofText</li>');
-//        this.input.val('');
-//        return false;
-//    });
-//};
-//
-//var populateList = function (){
-//    var listpublic = document.getElementById('list-public');
-//    list = new ServerClient({
-//        view     : listpublic
-//    });
-//};
 //
 //document.addEventListener('DOMContentLoaded', function() {
 //    console.log('HelloApp!' + '\n');
@@ -199,8 +37,13 @@ function openSettings (){
     chrome.tabs.create({ url: "http://localhost:3000" });
 }
 
-function setPause (){
-    console.log("Pressed Pause\n");
+function setPause (state){
+    var pauseFlag = state;
+    var input = document.getElementById('pause');
+    if(pauseFlag)
+        input.setAttribute('value', 'Unpause');
+    else
+        input.setAttribute('value', 'Pause');
 }
 
 function renderHeader(){
@@ -220,7 +63,37 @@ function renderHeader(){
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    //renderHeader();
-    openSettings();
+    var pausedFlag = 0;
+    renderHeader();
+    var settings = document.getElementById('settings');
+    var pause = document.getElementById('pause');
+
+    if(1){
+        var div = document.getElementById('status');
+        //GET USERNAME FROM COOKIES
+        var cookieUser = "dcostigan@umass.edu";
+        var text = document.createTextNode(cookieUser);
+        div.appendChild(text);
+        var input = document.getElementById('sign');
+        input.setAttribute('value', 'Sign Out');
+    }
+    else {
+        var div = document.getElementById('status');
+        var input = document.getElementById('sign');
+        input.setAttribute('value', 'Sign In');
+    }
+
+    settings.addEventListener('click', function (){
+        openSettings();
+    });
+
+    pause.addEventListener('click', function (){
+        console.log("Entered Paused State");
+        pausedFlag = !pausedFlag;
+        setPause(pausedFlag);
+    });
+
+}, function(errorMessage) {
+    console.log(errorMessage);
 });
 
